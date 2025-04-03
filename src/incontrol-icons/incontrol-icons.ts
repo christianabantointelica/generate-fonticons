ef3540feda8f2353654850b987735e2fc9867219
Christian Abanto <christian.abanto@intelica.com>
2025-04-03 09:40:20 -0500

    config generate fonticons

---
+++ src/incontrol-icons/incontrol-icons.ts
@@ -0,0 +1,345 @@
+export type IncontrolIconsId =
+  | "add"
+  | "arrow-down"
+  | "arrow-left"
+  | "arrow-right"
+  | "arrow-up"
+  | "back-circle"
+  | "bank"
+  | "bars"
+  | "business-intelligence"
+  | "calendar-regular"
+  | "calendar"
+  | "card-management"
+  | "card-network-billing"
+  | "card-network-interchange"
+  | "card-terminal"
+  | "caret-down"
+  | "caret-up"
+  | "chained"
+  | "chart"
+  | "check-circle"
+  | "check"
+  | "clone-regular"
+  | "close-circle"
+  | "close1"
+  | "collapse-down"
+  | "collapse-rows"
+  | "collapse-up"
+  | "contract-screen"
+  | "copy"
+  | "dashboard-intelica"
+  | "dashboard"
+  | "delete"
+  | "document"
+  | "download"
+  | "edit"
+  | "expand-rows"
+  | "expand-screen"
+  | "external-link"
+  | "fast-forward"
+  | "fast-reverse"
+  | "favorite"
+  | "file-exchange"
+  | "filter-disabled"
+  | "filter"
+  | "foward-circle"
+  | "gears-solid"
+  | "help-circle"
+  | "home"
+  | "id-card-regular"
+  | "images"
+  | "info-circle"
+  | "list-options"
+  | "list-solid"
+  | "logout"
+  | "merchant-management"
+  | "merchant-profitability"
+  | "mv-interchange"
+  | "no-view"
+  | "pencil-solid"
+  | "portfolio-management"
+  | "preview"
+  | "rbi-internal"
+  | "refresh"
+  | "regional-preview"
+  | "remove1"
+  | "search"
+  | "security"
+  | "settings"
+  | "star-o"
+  | "star"
+  | "terms"
+  | "times"
+  | "unchained"
+  | "unit-costs"
+  | "upload-fill"
+  | "upload"
+  | "user-solid"
+  | "user"
+  | "users-rectangle-solid"
+  | "view"
+  | "warning"
+  | "world"
+  | "zoom-in"
+  | "zoom-out";
+
+export type IncontrolIconsKey =
+  | "Add"
+  | "ArrowDown"
+  | "ArrowLeft"
+  | "ArrowRight"
+  | "ArrowUp"
+  | "BackCircle"
+  | "Bank"
+  | "Bars"
+  | "BusinessIntelligence"
+  | "CalendarRegular"
+  | "Calendar"
+  | "CardManagement"
+  | "CardNetworkBilling"
+  | "CardNetworkInterchange"
+  | "CardTerminal"
+  | "CaretDown"
+  | "CaretUp"
+  | "Chained"
+  | "Chart"
+  | "CheckCircle"
+  | "Check"
+  | "CloneRegular"
+  | "CloseCircle"
+  | "Close1"
+  | "CollapseDown"
+  | "CollapseRows"
+  | "CollapseUp"
+  | "ContractScreen"
+  | "Copy"
+  | "DashboardIntelica"
+  | "Dashboard"
+  | "Delete"
+  | "Document"
+  | "Download"
+  | "Edit"
+  | "ExpandRows"
+  | "ExpandScreen"
+  | "ExternalLink"
+  | "FastForward"
+  | "FastReverse"
+  | "Favorite"
+  | "FileExchange"
+  | "FilterDisabled"
+  | "Filter"
+  | "FowardCircle"
+  | "GearsSolid"
+  | "HelpCircle"
+  | "Home"
+  | "IdCardRegular"
+  | "Images"
+  | "InfoCircle"
+  | "ListOptions"
+  | "ListSolid"
+  | "Logout"
+  | "MerchantManagement"
+  | "MerchantProfitability"
+  | "MvInterchange"
+  | "NoView"
+  | "PencilSolid"
+  | "PortfolioManagement"
+  | "Preview"
+  | "RbiInternal"
+  | "Refresh"
+  | "RegionalPreview"
+  | "Remove1"
+  | "Search"
+  | "Security"
+  | "Settings"
+  | "StarO"
+  | "Star"
+  | "Terms"
+  | "Times"
+  | "Unchained"
+  | "UnitCosts"
+  | "UploadFill"
+  | "Upload"
+  | "UserSolid"
+  | "User"
+  | "UsersRectangleSolid"
+  | "View"
+  | "Warning"
+  | "World"
+  | "ZoomIn"
+  | "ZoomOut";
+
+export enum IncontrolIcons {
+  Add = "add",
+  ArrowDown = "arrow-down",
+  ArrowLeft = "arrow-left",
+  ArrowRight = "arrow-right",
+  ArrowUp = "arrow-up",
+  BackCircle = "back-circle",
+  Bank = "bank",
+  Bars = "bars",
+  BusinessIntelligence = "business-intelligence",
+  CalendarRegular = "calendar-regular",
+  Calendar = "calendar",
+  CardManagement = "card-management",
+  CardNetworkBilling = "card-network-billing",
+  CardNetworkInterchange = "card-network-interchange",
+  CardTerminal = "card-terminal",
+  CaretDown = "caret-down",
+  CaretUp = "caret-up",
+  Chained = "chained",
+  Chart = "chart",
+  CheckCircle = "check-circle",
+  Check = "check",
+  CloneRegular = "clone-regular",
+  CloseCircle = "close-circle",
+  Close1 = "close1",
+  CollapseDown = "collapse-down",
+  CollapseRows = "collapse-rows",
+  CollapseUp = "collapse-up",
+  ContractScreen = "contract-screen",
+  Copy = "copy",
+  DashboardIntelica = "dashboard-intelica",
+  Dashboard = "dashboard",
+  Delete = "delete",
+  Document = "document",
+  Download = "download",
+  Edit = "edit",
+  ExpandRows = "expand-rows",
+  ExpandScreen = "expand-screen",
+  ExternalLink = "external-link",
+  FastForward = "fast-forward",
+  FastReverse = "fast-reverse",
+  Favorite = "favorite",
+  FileExchange = "file-exchange",
+  FilterDisabled = "filter-disabled",
+  Filter = "filter",
+  FowardCircle = "foward-circle",
+  GearsSolid = "gears-solid",
+  HelpCircle = "help-circle",
+  Home = "home",
+  IdCardRegular = "id-card-regular",
+  Images = "images",
+  InfoCircle = "info-circle",
+  ListOptions = "list-options",
+  ListSolid = "list-solid",
+  Logout = "logout",
+  MerchantManagement = "merchant-management",
+  MerchantProfitability = "merchant-profitability",
+  MvInterchange = "mv-interchange",
+  NoView = "no-view",
+  PencilSolid = "pencil-solid",
+  PortfolioManagement = "portfolio-management",
+  Preview = "preview",
+  RbiInternal = "rbi-internal",
+  Refresh = "refresh",
+  RegionalPreview = "regional-preview",
+  Remove1 = "remove1",
+  Search = "search",
+  Security = "security",
+  Settings = "settings",
+  StarO = "star-o",
+  Star = "star",
+  Terms = "terms",
+  Times = "times",
+  Unchained = "unchained",
+  UnitCosts = "unit-costs",
+  UploadFill = "upload-fill",
+  Upload = "upload",
+  UserSolid = "user-solid",
+  User = "user",
+  UsersRectangleSolid = "users-rectangle-solid",
+  View = "view",
+  Warning = "warning",
+  World = "world",
+  ZoomIn = "zoom-in",
+  ZoomOut = "zoom-out",
+}
+
+export const INCONTROL_ICONS_CODEPOINTS: { [key in IncontrolIcons]: string } = {
+  [IncontrolIcons.Add]: "61697",
+  [IncontrolIcons.ArrowDown]: "61698",
+  [IncontrolIcons.ArrowLeft]: "61699",
+  [IncontrolIcons.ArrowRight]: "61700",
+  [IncontrolIcons.ArrowUp]: "61701",
+  [IncontrolIcons.BackCircle]: "61702",
+  [IncontrolIcons.Bank]: "61703",
+  [IncontrolIcons.Bars]: "61704",
+  [IncontrolIcons.BusinessIntelligence]: "61705",
+  [IncontrolIcons.CalendarRegular]: "61706",
+  [IncontrolIcons.Calendar]: "61707",
+  [IncontrolIcons.CardManagement]: "61708",
+  [IncontrolIcons.CardNetworkBilling]: "61709",
+  [IncontrolIcons.CardNetworkInterchange]: "61710",
+  [IncontrolIcons.CardTerminal]: "61711",
+  [IncontrolIcons.CaretDown]: "61712",
+  [IncontrolIcons.CaretUp]: "61713",
+  [IncontrolIcons.Chained]: "61714",
+  [IncontrolIcons.Chart]: "61715",
+  [IncontrolIcons.CheckCircle]: "61716",
+  [IncontrolIcons.Check]: "61717",
+  [IncontrolIcons.CloneRegular]: "61718",
+  [IncontrolIcons.CloseCircle]: "61719",
+  [IncontrolIcons.Close1]: "61720",
+  [IncontrolIcons.CollapseDown]: "61721",
+  [IncontrolIcons.CollapseRows]: "61722",
+  [IncontrolIcons.CollapseUp]: "61723",
+  [IncontrolIcons.ContractScreen]: "61724",
+  [IncontrolIcons.Copy]: "61725",
+  [IncontrolIcons.DashboardIntelica]: "61726",
+  [IncontrolIcons.Dashboard]: "61727",
+  [IncontrolIcons.Delete]: "61728",
+  [IncontrolIcons.Document]: "61729",
+  [IncontrolIcons.Download]: "61730",
+  [IncontrolIcons.Edit]: "61731",
+  [IncontrolIcons.ExpandRows]: "61732",
+  [IncontrolIcons.ExpandScreen]: "61733",
+  [IncontrolIcons.ExternalLink]: "61734",
+  [IncontrolIcons.FastForward]: "61735",
+  [IncontrolIcons.FastReverse]: "61736",
+  [IncontrolIcons.Favorite]: "61737",
+  [IncontrolIcons.FileExchange]: "61738",
+  [IncontrolIcons.FilterDisabled]: "61739",
+  [IncontrolIcons.Filter]: "61740",
+  [IncontrolIcons.FowardCircle]: "61741",
+  [IncontrolIcons.GearsSolid]: "61742",
+  [IncontrolIcons.HelpCircle]: "61743",
+  [IncontrolIcons.Home]: "61744",
+  [IncontrolIcons.IdCardRegular]: "61745",
+  [IncontrolIcons.Images]: "61746",
+  [IncontrolIcons.InfoCircle]: "61747",
+  [IncontrolIcons.ListOptions]: "61748",
+  [IncontrolIcons.ListSolid]: "61749",
+  [IncontrolIcons.Logout]: "61750",
+  [IncontrolIcons.MerchantManagement]: "61751",
+  [IncontrolIcons.MerchantProfitability]: "61752",
+  [IncontrolIcons.MvInterchange]: "61753",
+  [IncontrolIcons.NoView]: "61754",
+  [IncontrolIcons.PencilSolid]: "61755",
+  [IncontrolIcons.PortfolioManagement]: "61756",
+  [IncontrolIcons.Preview]: "61757",
+  [IncontrolIcons.RbiInternal]: "61758",
+  [IncontrolIcons.Refresh]: "61759",
+  [IncontrolIcons.RegionalPreview]: "61760",
+  [IncontrolIcons.Remove1]: "61761",
+  [IncontrolIcons.Search]: "61762",
+  [IncontrolIcons.Security]: "61763",
+  [IncontrolIcons.Settings]: "61764",
+  [IncontrolIcons.StarO]: "61765",
+  [IncontrolIcons.Star]: "61766",
+  [IncontrolIcons.Terms]: "61767",
+  [IncontrolIcons.Times]: "61768",
+  [IncontrolIcons.Unchained]: "61769",
+  [IncontrolIcons.UnitCosts]: "61770",
+  [IncontrolIcons.UploadFill]: "61771",
+  [IncontrolIcons.Upload]: "61772",
+  [IncontrolIcons.UserSolid]: "61773",
+  [IncontrolIcons.User]: "61774",
+  [IncontrolIcons.UsersRectangleSolid]: "61775",
+  [IncontrolIcons.View]: "61776",
+  [IncontrolIcons.Warning]: "61777",
+  [IncontrolIcons.World]: "61778",
+  [IncontrolIcons.ZoomIn]: "61779",
+  [IncontrolIcons.ZoomOut]: "61780",
+};
